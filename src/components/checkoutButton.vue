0e84a61d67d2294229a388d014e0906c3bbf57f8
Jater <p833795p@gmail.com>
2022-02-11 18:58:41 +0800

    feat: build checkoutButton component.

---
+++ src/components/checkoutButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="main__button justify-content-flex-end">
+    <div class="main__button__previous d-none">
+      <button class="previous">← 上一步</button>
+    </div>
+    <div class="main__button__next stepOne">
+      <button class="next">下一步 →</button>
+    </div>
+  </div>
+</template>
--- src/views/checkoutAddress.vue
+++ src/views/checkoutAddress.vue
@@ -3,18 +3,20 @@
     <h1 class="main__title">結帳</h1>
     <Stepper />
     <Cart />
-    
+    <Button />
   </main>   
 </template>
 
 <script>
 import Stepper from '../components/stepper.vue'
 import Cart from '../components/cart.vue'
+import Button from '../components/checkoutButton.vue'
 
 export default {
   components: {
     Stepper,
-    Cart
+    Cart,
+    Button
   }
 }
 </script>
